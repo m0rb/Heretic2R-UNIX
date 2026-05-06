1f7fccfc9dacbcd76934e847dd1de0e5e36c528f
m-x-d <j.maxed@gmail.com>
2026-05-06 18:48:56 +0300

    Cosmetic tweaks in g_func_Utility.h.

--- src/game/src/GameObjects/g_func_Utility.h
+++ src/game/src/GameObjects/g_func_Utility.h
@@ -13,8 +13,8 @@
 #define STATE_UP			2
 #define STATE_DOWN			3
 
-extern void MoveCalc(edict_t* ent, const vec3_t dest, void(*func)(edict_t*));
-extern void AngleMoveCalc(edict_t* ent, void(*func)(edict_t*));
+extern void MoveCalc(edict_t* ent, const vec3_t dest, void (*func)(edict_t*));
+extern void AngleMoveCalc(edict_t* ent, void (*func)(edict_t*));
 extern void FuncTrainAngleMoveCalc(edict_t* self, const edict_t* ent, const vec3_t dest);
 extern void FuncPlayMoveStartSound(edict_t* ent); //mxd
 extern void FuncPlayMoveEndSound(edict_t* ent); //mxd
