3c2c72a57e54cc05d2ef713c14793f964714083d
m-x-d <j.maxed@gmail.com>
2026-08-05 15:28:22 +0300

    ShadowAddToView(), ShadowReferenceAddToView(): use CTF_LERP_BMODEL_ORIGIN flag (fixes shadows z-jittering when standing on a moving lift).

--- src/client effects/src/Effects/fx_Shadow.c
+++ src/client effects/src/Effects/fx_Shadow.c
@@ -32,7 +32,10 @@ static qboolean ShadowAddToView(client_entity_t* self, centity_t* owner)
 
 	// Determine visibility.
 	trace_t trace;
-	fxi.Trace(start_pos, vec3_origin, vec3_origin, end_pos, CONTENTS_SOLID, CTF_CLIP_TO_WORLD | CTF_CLIP_TO_BMODELS, &trace); //mxd. CEF_CLIP_TO_WORLD in original logic. Fixes shadows disappearing when standing on brush entities (like func_wall or func_breakable).
+
+	//mxd. +CTF_CLIP_TO_BMODELS (fixes shadows disappearing when standing on brush entities (like func_wall or func_breakable)).
+	//mxd. +CTF_LERP_BMODEL_ORIGIN (fixes shadows z-jittering when standing on a moving lift).
+	fxi.Trace(start_pos, vec3_origin, vec3_origin, end_pos, CONTENTS_SOLID, CTF_CLIP_TO_WORLD | CTF_CLIP_TO_BMODELS | CTF_LERP_BMODEL_ORIGIN, &trace);
 
 	// In air or in solid, so no shadow.
 	if (trace.startsolid || trace.fraction == 1.0f)
@@ -106,7 +109,10 @@ static qboolean ShadowReferenceAddToView(client_entity_t* self, centity_t* owner
 
 	// Determine visibility.
 	trace_t trace;
-	fxi.Trace(start_pos, vec3_origin, vec3_origin, end_pos, CONTENTS_SOLID, CTF_CLIP_TO_WORLD | CTF_CLIP_TO_BMODELS, &trace); //mxd. CEF_CLIP_TO_WORLD in original logic. Fixes shadows disappearing when standing on brush entities (like func_wall or func_breakable).
+
+	//mxd. +CTF_CLIP_TO_BMODELS (fixes shadows disappearing when standing on brush entities (like func_wall or func_breakable)).
+	//mxd. +CTF_LERP_BMODEL_ORIGIN (fixes shadows z-jittering when standing on a moving lift).
+	fxi.Trace(start_pos, vec3_origin, vec3_origin, end_pos, CONTENTS_SOLID, CTF_CLIP_TO_WORLD | CTF_CLIP_TO_BMODELS | CTF_LERP_BMODEL_ORIGIN, &trace);
 
 	// In air or in solid, so no shadow.
 	if (trace.startsolid || trace.fraction == 1.0f)
